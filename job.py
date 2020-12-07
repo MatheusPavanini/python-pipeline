13de0997f7fb1c018216f94fba085d4b8450b8e0
MatheusPavanini <matheus.ppj@gmail.com>
2020-12-06 21:01:13 -0300

    teste pipeline dnv

--- job.py
+++ job.py
@@ -2,7 +2,8 @@ import os, sys, time
 def job():
     print("Now running")
     print("Now running2")
-    print("Now running2")
+    print("Now running3")
+    print("Now running4")
 def main():
     while True:
         job()
